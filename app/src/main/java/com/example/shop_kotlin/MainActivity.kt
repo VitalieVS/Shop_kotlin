5f004d06d7aacb1d1a2c36f2ac4cfb1aed5e93e7
VitalieVS <vitaliesandiuc@gmail.com>
2022-03-19 14:01:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shop_kotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.shop_kotlin
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.shop_kotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+}
